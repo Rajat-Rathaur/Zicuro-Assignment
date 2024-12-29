625dbf512d94a215709939e873265bfa577e834c
Rajat-Rathaur <142532314+Rajat-Rathaur@users.noreply.github.com>
2024-12-29 20:50:20 +0530

    removed unncessary code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import EditorComponent from "./EditorComponent";
 import Title from "./components/Title";
 
 const App = () => {
-  const [editorContent, setEditorContent] = useState(null);
-
-  useEffect(() => {
-    const savedContent = localStorage.getItem("editorContent");
-    if (savedContent) {
-      try {
-        const parsedContent = JSON.parse(savedContent);
-        setEditorContent(parsedContent);
-      } catch (error) {
-        console.error("Error parsing saved content:", error);
-      }
-    }
-  }, []);
-
-  const handleSave = (content) => {
-    localStorage.setItem("editorContent", JSON.stringify(content));
-    setEditorContent(content); 
-  };
-
   return (
     <div className="App">
       <Title />
       <div className="editor-container " style={{ border: "1px solid #507dad", padding: "10px" }}>
-        <EditorComponent initialContent={editorContent} onSave={handleSave}        />
+        <EditorComponent  />
       </div>
     </div>
   );
